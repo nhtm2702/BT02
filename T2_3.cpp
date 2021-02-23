1e2ca8a7e0b71e43a12c79f9efb2540ebd41b527
NHTMy <20020223@vnu.edu.vn>
2021-02-23 16:04:27 +0700

    Initial commit.

---
+++ T2_3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int d=-1;
+    for (int i=0;i<5;i++)
+    {
+        d++;
+        for (int j=0;j<4-d;j++) cout << " ";
+        for (int j=4-d;j<=4+d;j++) cout << "*";
+        for (int j=5+d;j<=10;j++) cout << " ";
+        cout << endl;
+    }
+    return 0;
+}
